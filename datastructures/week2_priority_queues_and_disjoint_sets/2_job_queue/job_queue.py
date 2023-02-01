43858107ab7c7541ed006c21caacc41abe3347c0
Philip Lo <lophilip@outlook.com>
2023-02-01 08:36:49 -0700

    finished question 2 on week 2  Changes to be committed: 	new file:   datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/__pycache__/job_queue.cpython-310.pyc 	modified:   datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py 	modified:   datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/test_job_queue.py

--- datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -21,18 +21,138 @@ def assign_jobs_native(n_workers, jobs):
 
 
 
-workertuple = namedtuple("worker", ["worker", "free_at"])
+class workertype():
+    def __init__(self,worker,free_at):
+        self.worker=worker
+        self.free_at=free_at
 
 class workerclass:
     def __init__(self,nworker):
         self.worker_heap=[]
         
         for i in range (0,nworker):
-            self.worker_heap.append(workertuple(i,0))
+            #self.worker_heap.append(workertype(i,0))
+            self.insert(workertype(i,0))
             
+    def shiftdown(self,i):
+        minindex=i                        
+        
+        l=2*i+1
+        if l<len(self.worker_heap) and (self.worker_heap[l].free_at<self.worker_heap[minindex].free_at
+                                        or (self.worker_heap[l].free_at==self.worker_heap[minindex].free_at and 
+                                            self.worker_heap[l].worker<self.worker_heap[minindex].worker)) :
+            minindex=l
+            
+        r=2*i+2
+        if r<len(self.worker_heap) and (self.worker_heap[r].free_at<self.worker_heap[minindex].free_at
+                                        or (self.worker_heap[r].free_at==self.worker_heap[minindex].free_at and
+                                            self.worker_heap[r].worker<self.worker_heap[minindex].worker)) :
+            minindex=r
+        
+        if i!=minindex:
+            self.worker_heap[i],self.worker_heap[minindex]=self.worker_heap[minindex],self.worker_heap[i]
+            self.shiftdown(minindex) 
+    
+    def shiftup(self,i):
+        if i==0:
+            return
+        p=(i-1)//2
+        if (self.worker_heap[p].free_at>self.worker_heap[i].free_at or 
+                (self.worker_heap[p].free_at==self.worker_heap[i].free_at and 
+                 self.worker_heap[p].worker>self.worker_heap[i].worker)):
+            self.worker_heap[p],self.worker_heap[i]=self.worker_heap[i],self.worker_heap[p]
+            self.shiftup(p)
+            
+    def insert(self,i):
+        self.worker_heap.append(i)
+        self.shiftup(len(self.worker_heap)-1)
+        
+    def extractmin(self):
+        result=self.worker_heap[0]
+        self.worker_heap[0]=self.worker_heap[len(self.worker_heap)-1]        
+        self.worker_heap.pop()
+        self.shiftdown(0)
+        return result
+    
+    def num_worker(self):
+        return len(self.worker_heap)
+    
+    def free_at(self):
+        if len(self.worker_heap)==0:
+            return float('inf')
+        return self.worker_heap[0].free_at
+
+class workerclass2:
+    #uses priority queue
+    def __init__(self,nworker):
+        self.worker_heap=[]
+        
+        for i in range (0,nworker):
+            #self.worker_heap.append(workertype(i,0))
+            self.insert(workertype(i,0))            
+        
+    def extractmin(self):
+        result=self.worker_heap[0]
+        self.worker_heap=self.worker_heap[1:]
+        return result
+    
+    def insert(self,i):
+        self.worker_heap.append(i)
+        self.worker_heap.sort(key=lambda x: x.free_at)
+        
+
+    
+    
+def assign_jobs_ot1(n_workers, jobs):
+    workers=workerclass(n_workers)
+    result=[]
+    for i in jobs:
+        w=workers.extractmin()
+        start_time=w.free_at
+        w.free_at+=i
+        workers.insert(w)
+        result.append(AssignedJob(w.worker, start_time))
+    return result
+
+
+
+    
+def assign_jobs_ot2(n_workers, jobs):
+    workers=workerclass2(n_workers)
+    result=[]
+    for i in jobs:
+        w=workers.extractmin()
+        start_time=w.free_at
+        w.free_at+=i
+        workers.insert(w)
+        result.append(AssignedJob(w.worker, start_time))
+    return result
+
+def assign_jobs_ot3(n_workers, jobs):
+    worker1=workerclass(n_workers)
+    worker2=workerclass(0)
+    
+    freeworker=worker1
+    busyworker=worker2
+    result=[]
+    for i in jobs:
+        
+        if freeworker.free_at()>busyworker.free_at() and busyworker.num_worker()>0:
+            freeworker,busyworker=busyworker,freeworker
+            
+        w=freeworker.extractmin()
+        start_time=w.free_at
+        w.free_at+=i
+        busyworker.insert(w)
+        result.append(AssignedJob(w.worker, start_time))
+    return result
+
 
 def assign_jobs(n_workers, jobs):
-    return assign_jobs_native(n_workers,jobs)
+    #return assign_jobs_native(n_workers,jobs)
+    return assign_jobs_ot1(n_workers,jobs)
+    #return assign_jobs_ot3(n_workers,jobs)
+    
 
 def main():
     n_workers, n_jobs = map(int, input().split())
--- datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/test_job_queue.py
+++ datastructures/week2_priority_queues_and_disjoint_sets/2_job_queue/test_job_queue.py
@@ -1,17 +1,114 @@
 from job_queue import *
+import random
+from time import time
 
 def test1():
     nworker=2
     jobs=[1,2,3,4,4]
     
-    job=assign_jobs(nworker,jobs)
+    job=assign_jobs_native(nworker,jobs)
+    job_ot=assign_jobs(nworker,jobs)
     
-    print(job)
+    #print(job)
+    #print(job_ot)
     
     assert job == [AssignedJob(worker=0, started_at=0), AssignedJob(worker=1, started_at=0), AssignedJob(worker=0, started_at=1), AssignedJob(worker=1, started_at=2), AssignedJob(worker=0, started_at=4)]
+    assert job == job_ot
+    
+    print('test1 passed')
+
+
+
+
+
+def test2():
+    nworker=10
+    jobs=[]
+    
+    for i in range(0,10000):
+        jobs.append(random.randint(1,10))
+        
+    n1=time()
+    job_nt=assign_jobs_native(nworker,jobs)
+    n2=time()    
+    native=n2-n1
+    
+    ot1=time()
+    job_ot=assign_jobs(nworker,jobs)
+    ot2=time()
+    ot=ot2-ot1
+    
+    print('native: ',native)
+    print('ot: ',ot)
+    
+    print('ot/native: ',ot/native)
+    
+    #assert ot/native < 0.2 ,'ot is more than 20% slower than native'
+    
+    #print (job_nt)
+    #print (job_ot)
+        
+    assert len(job_nt) == len(job_ot)
+    assert job_nt == job_ot
+    
+    for x in range(0,len(job_nt)):
+        #assert job_nt[x].worker == job_ot[x].worker
+        if job_nt[x].started_at != job_ot[x].started_at:
+            print('x: ',x)
+            print('job_nt[x].started_at: ',job_nt[x].started_at)
+            print('job_ot[x].started_at: ',job_ot[x].started_at)
+        assert job_nt[x].started_at == job_ot[x].started_at, 'job_nt[x].started_at != job_ot[x].started_at'
+    
+    print('test2 passed')    
+    
+
+def test3():
+    nworker=4
+    jobs=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    
+    job_nt=assign_jobs_native(nworker,jobs)
+    job_ot=assign_jobs(nworker,jobs)
+    
+    print(job_nt)
+    print(job_ot)
+    
+    assert job_nt == job_ot,'job_nt != job_ot'
+    
+    """
+    for x in range(0,len(job_nt)):        
+        assert job_nt[x].started_at == job_ot[x].started_at
+    """
+    print('test3 passed')
+
+
+
+def test_workerclass():
+    workertype=workerclass(10)
+        
+    """
+    workertype.extractmin()           
+    for i in workertype.worker_heap:
+        print (i.worker)
+    print (workertype.worker_heap)
     
     
+    workertype.extractmin()
     
+    for i in workertype.worker_heap:
+        print (i.worker)
+    print (workertype.worker_heap)
+    """
+    
+    for i in range(0,10):
+        w=workertype.extractmin()
+        print(w.worker)
+        
 if __name__=='__main__':
-    test1()
+    #test1()
+    #test2()
+    test3()
+    
+    #test_workerclass()
+    
+    
     
