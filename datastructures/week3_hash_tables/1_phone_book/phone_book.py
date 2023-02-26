bcbd3ebecd071eb5597a22c545d748fc0092411d
Philip Lo <lophilip@outlook.com>
2023-02-25 17:29:02 -0700

    Changes to be committed: 	modified:   datastructures/week3_hash_tables/1_phone_book/phone_book.py 	modified:   datastructures/week3_hash_tables/2_hash_chains/hash_chains.py

--- datastructures/week3_hash_tables/1_phone_book/phone_book.py
+++ datastructures/week3_hash_tables/1_phone_book/phone_book.py
@@ -7,6 +7,23 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
+class query_direct: #direct addressing
+    def __init__(self):
+        self.phone_book=[]
+        self.init_phone_book()
+    def init_phone_book(self,max_number=pow(10,7)):
+        """initialize phone book with max_number of None values"""
+        self.phone_book=[None]*max_number
+    def add(self,number,name):
+        self.phone_book[number]=name
+    def del_num(self,number):
+        self.phone_book[number]=None
+    def find(self,number):
+        if number <= len(self.phone_book):
+            return self.phone_book[number]
+        else:
+            return None
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
@@ -42,6 +59,27 @@ def process_queries(queries):
             result.append(response)
     return result
 
+def process_queries_direct(queries):
+    result = []
+    # Keep list of all existing (i.e. not deleted yet) contacts.
+    contacts = query_direct()
+    for cur_query in queries:
+        if cur_query.type == 'add':
+            # if we already have contact with such number,
+            # we should rewrite contact's name
+            contacts.add(cur_query.number,cur_query.name)
+        elif cur_query.type == 'del':
+            contacts.del_num(cur_query.number)
+        elif cur_query.type == 'find':
+            response = contacts.find(cur_query.number)
+            if response is None:
+                response = 'not found'
+            result.append(response)
+        else:
+            print('unknown query type')
+            assert(True), "unknown query type"
+    return result
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
+    #write_responses(process_queries(read_queries()))
+    write_responses(process_queries_direct(read_queries()))
 
--- datastructures/week3_hash_tables/2_hash_chains/hash_chains.py
+++ datastructures/week3_hash_tables/2_hash_chains/hash_chains.py
@@ -58,7 +58,128 @@ class QueryProcessor:
         for i in range(n):
             self.process_query(self.read_query())
 
+class hash_chain_class():
+    def __init__(self,num_buckets):
+        assert isinstance(num_buckets,int),'buckets should be int, and number of buckts'
+        self.buckets=[]
+        self.chain=[]
+        self.num_buckets=num_buckets
+        for i in range (0,num_buckets):
+            self.buckets.append(i)
+            self.chain.append([])
+    
+    def calculate_hash(self,thestring) -> int:
+        ans=0
+        multiplier = 263
+        prime = 1000000007
+        assert isinstance(thestring,str),'thestring should be a string, of which hash will be returned'
+
+        for c in reversed(thestring):
+            ans = (ans * self._multiplier + ord(c)) % self._prime
+        ans=ans%self.num_buckets
+
+        return ans
+    
+    def add_string(self,input) -> None:
+        hash=self.calculate_hash(input)
+        if hash<self.num_buckets:
+            self.chain[hash].append(input)
+        else:
+            self.chain[self.num_buckets-1].append(input)
+
+    def del_string(self,input) -> bool:
+        returnvalue=False
+
+        hash=self.calculate_hash(input)
+        list_string=[]
+        index=-1
+        if hash<self.num_buckets:
+            list_string=self.chain[hash]
+            index=hash
+        else:
+            list_string=self.chain[self.num_buckets-1]
+            index=self.num_buckets-1
+
+        list_string.reverse()
+
+        try:
+            string_index=list_string.index(input)
+        except ValueError:
+            string_index=-1
+
+        if string_index!=-1:
+            list_string.pop(string_index)
+            returnvalue=True
+            
+        list_string.reverse()
+
+        return returnvalue
+
+    def find_string(self,input) -> bool:
+        found=False
+
+        hash=self.calculate_hash(input)
+        index=-1
+        if hash<self.num_buckets:
+            index=hash
+        else:
+            index=self.num_buckets-1
+        
+        list_string=self.chain[index]
+
+        if input in list_string:
+            found=True
+
+        return found
+    
+    def check (self, index) -> list:
+        list_string=[]
+        if index<self.num_buckets:
+            list_string=list(self.chain[index])
+        else:
+            list_string=list(self.chain[self.num_buckets-1])
+
+        list_string.reverse()
+        return list_string
+
+class QueryProcessor_hash_chains:
+
+    def __init__(self, bucket_count):
+        self.hash_chain=hash_chain_class(bucket_count)
+
+
+ 
+    def process_query(self, query):
+        if query.type == "check":
+
+            list_string=self.hash_chain.check(query.ind)
+
+            printout=''
+            for x in list_string:
+                printout+=x
+                printout+=' '
+            printout=printout[-1]
+            print(printout)
+
+            
+        elif query.type == 'find':
+            found=self.hash_chain.find_string(query.s)
+            if found:
+                print('yes')
+            else:
+                print('no')
+        elif query.type == 'add':
+            self.hash_chain.add_string(query.s)
+        elif query.type == 'del':
+            self.hash_chain.del_string(query.s)
+        else:
+            print('error unknown query type %s'%query.type)
+
+    def process_queries(self):
+        n = int(input())
+        for i in range(n):
+            self.process_query(self.read_query())
 if __name__ == '__main__':
     bucket_count = int(input())
-    proc = QueryProcessor(bucket_count)
+    #proc = QueryProcessor(bucket_count)
     proc.process_queries()
