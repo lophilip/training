6d3527401ea9966dd8e045976713a832ea2c6ea0
Philip Lo <lophilip@outlook.com>
2023-04-10 17:22:32 -0600

    in progress  Changes to be committed: 	modified:   datastructures/week3_hash_tables/5_longest_common_substring/common_substring.py 	modified:   datastructures/week3_hash_tables/5_longest_common_substring/test_common_substring.py

--- datastructures/week3_hash_tables/5_longest_common_substring/common_substring.py
+++ datastructures/week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -56,6 +56,15 @@ class solver_hash_class:
         return string_equivalent
 
 
+    def compare_other_hashclass(self, other_hashclass, a, b, l):
+        assert isinstance(other_hashclass, solver_hash_class),'other_hashclass is not of type solver_hash_class'        
+        string_equivalent = True
+        h1_a, h2_a = self.calculte_hash(a, l)
+        h1_b, h2_b = other_hashclass.calculte_hash(b, l)
+                
+        if h1_a != h1_b or h2_a != h2_b:
+            string_equivalent = False
+        return string_equivalent
 
 
 def longest_common_substring(s1, s2):
@@ -67,26 +76,32 @@ def longest_common_substring(s1, s2):
     
     string1=solver_hash_class(s1)
     string2=solver_hash_class(s2)   
-    
 
+    #same=string1.compare_other_hashclass(string2, 1, 1, 3)
+    #f print(same)
+
+    
     # Use binary search to find the longest common substring.
-    max_len, max_i, max_j = 0, -1, -1
-    for i in range(len(s1)):
-        lo, hi = 0, min(len(s1)-i, len(s2))
-        while lo < hi:
-            mid = (lo+hi+1) // 2
-            h1 = (hash1[i+mid] - b**(mid-1)*hash1[mid-1]) % p
-            h2 = (hash2[i+mid] - b**(mid-1)*hash2[mid-1]) % p
-            if h1 == h2:
-                lo = mid
-            else:
-                hi = mid - 1
-        if lo > max_len:
-            max_len = lo
-            max_i = i
-            max_j = i+lo
-            
-        ans=Answer(max_i, max_j-max_len, max_len)
+    # a is long string --> hash table, b is short string --> hash dict
+    low = 0
+    high = min(len(s1), len(s2))
+    aStart, bStart, max_length = 0, 0, 0
+
+    while low <= high:
+        mid = (low + high) // 2  # mid is the length of the tested common substring
+        if low > high:
+            return aStart, bStart, max_length
+       
+        if check1 and check2:
+            for a, b in matches1.items():
+                temp = matches2.get(a, -1)
+                if temp != -1:
+                    max_length = mid
+                    aStart, bStart = a, b
+                    del aHash1, aHash2, bHash1, bHash2, matches1, matches2
+                    return MaxLength(string_a, string_b, mid + 1, high, max_length, aStart, bStart)
+        return MaxLength(string_a, string_b, low, mid - 1, max_length, aStart, bStart)
+    
 
     return ans
     
--- datastructures/week3_hash_tables/5_longest_common_substring/test_common_substring.py
+++ datastructures/week3_hash_tables/5_longest_common_substring/test_common_substring.py
@@ -6,6 +6,8 @@ def test1():
     
     answer2=longest_common_substring('cool','toolbox')    
     print(answer2)
+
+    assert answer==answer2, 'test 1 fail'
     
 if __name__=='__main__':
     test1()
