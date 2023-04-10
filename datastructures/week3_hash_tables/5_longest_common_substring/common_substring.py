7fa7e47a96643f3c2c28dc6617d54027fe233029
Philip Lo <lophilip@outlook.com>
2023-04-10 13:47:23 -0600

    trying to solve problem 5  Changes to be committed: 	modified:   datastructures/week3_hash_tables/5_longest_common_substring/common_substring.py

--- datastructures/week3_hash_tables/5_longest_common_substring/common_substring.py
+++ datastructures/week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -14,8 +14,83 @@ def solve(s, t):
 					ans = Answer(i, j, l)
 	return ans
 
+class solver_hash_class:
+    def __init__(self, s):
+        self.s = s
+        
+        self.x = 263
+        self.m1 = 1000000007
+        self.m2 = 1000000009
+        
+        self.calc_hash_precalculation()
+
+
+    def calc_hash_precalculation(self):
+        self.h1 = [0] * (len(self.s)+1)
+        self.h2 = [0] * (len(self.s)+1)        
+
+        for i in range(1, len(self.s)+1):
+            self.h1[i] = (self.h1[i - 1] * self.x + ord(self.s[i-1])) % self.m1
+                        
+            self.h2[i] = (self.h2[i - 1] * self.x + ord(self.s[i-1])) % self.m2
+
+        
+            
+    def calculte_hash(self, a, l):
+        y1 = pow(self.x, l, self.m1)
+        y2= pow(self.x, l, self.m2)
+        
+        h1=(self.h1[a+l]-self.h1[a]*y1)%self.m1
+        
+        h2=(self.h2[a+l]-self.h2[a]*y2)%self.m2        
+        
+        return h1, h2
+    
+    def ask(self, a, b, l):        
+        string_equivalent = True
+        h1_a, h2_a = self.calculte_hash(a, l)
+        h1_b, h2_b = self.calculte_hash(b, l)
+                
+        if h1_a != h1_b or h2_a != h2_b:
+            string_equivalent = False
+        return string_equivalent
+
+
+
 
 def longest_common_substring(s1, s2):
+    """
+    Returns the start indices and length of the longest common substring of s1 and s2.
+    """
+    if not s1 or not s2:
+        return 0, 0, 0
+    
+    string1=solver_hash_class(s1)
+    string2=solver_hash_class(s2)   
+    
+
+    # Use binary search to find the longest common substring.
+    max_len, max_i, max_j = 0, -1, -1
+    for i in range(len(s1)):
+        lo, hi = 0, min(len(s1)-i, len(s2))
+        while lo < hi:
+            mid = (lo+hi+1) // 2
+            h1 = (hash1[i+mid] - b**(mid-1)*hash1[mid-1]) % p
+            h2 = (hash2[i+mid] - b**(mid-1)*hash2[mid-1]) % p
+            if h1 == h2:
+                lo = mid
+            else:
+                hi = mid - 1
+        if lo > max_len:
+            max_len = lo
+            max_i = i
+            max_j = i+lo
+            
+        ans=Answer(max_i, max_j-max_len, max_len)
+
+    return ans
+    
+def longest_common_substring_original(s1, s2):
     """
     Returns the start indices and length of the longest common substring of s1 and s2.
     """
@@ -54,8 +129,6 @@ def longest_common_substring(s1, s2):
         ans=Answer(max_i, max_j-max_len, max_len)
 
     return ans
-    
-
         
             
 
