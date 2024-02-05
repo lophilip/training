8df2c1866129e1efaea5544414a17c4517e95f37
Philip Lo <lophilip@outlook.com>
2024-02-04 21:34:59 -0700

    in progress graphs week 2 assigment question2  Changes to be committed: 	modified:   graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py 	new file:   graphs/week2_graph_decomposition2/1_cs_curriculum/alternative_acyclicity.py 	new file:   graphs/week2_graph_decomposition2/1_cs_curriculum/test_acyclicity.py 	new file:   graphs/week2_graph_decomposition2/2_order_of_courses/test_topsort.py 	modified:   graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py 	new file:   graphs/week2_graph_decomposition2/_dc4aebf17f30b5504eb92843c2d9e0fc_09_graph_decomposition_6_dags.pdf 	new file:   graphs/week2_graph_decomposition2/_dc4aebf17f30b5504eb92843c2d9e0fc_09_graph_decomposition_7_topological-sort.pdf 	new file:   graphs/week2_graph_decomposition2/_dc4aebf17f30b5504eb92843c2d9e0fc_09_graph_decomposition_8_strongly-connected-components.pdf 	new file:   graphs/week2_graph_decomposition2/_dc4aebf17f30b5504eb92843c2d9e0fc_09_graph_decomposition_9_computing-sccs.pdf

--- graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
+++ graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
@@ -2,11 +2,191 @@
 
 import sys
 
+class graphnodeclass:
+    def __init__(self,n):
+        self.adj=[]        
+        self.removed=False  #flags to indicate if node has been removed
+        self.index=n    #index of node in the list
+    def add_adj(self,adj):
+        if adj not in self.adj: #do not do duplicate adj
+            self.adj.append(adj)
 
-def acyclic(adj):
+    def remove_adj(self,n):
+        if n in self.adj:
+            self.adj.remove(n)
+
+
+def number_of_components(adj):
+    num_edges=len(adj)
+    num_vertices=-1
+    for i in adj:
+        for j in i:
+            if j>num_vertices:
+                num_vertices=j
+    num_vertices+=1
+
+    return num_vertices,num_edges
+
+
+def transverse_withoutbacktracking(node,index,visited=[], path=[]): #pass test 1 to 10, fail 11
+    sink=False
+    repeat=False
+
+    currentnode=node[index]
+
+    if index not in visited:
+            visited.append(index)           
+    
+    if currentnode.adj==[]:
+        sink=True
+    else:        
+        for i in currentnode.adj:            
+            if repeat:
+                break
+            if i not in visited and repeat==False:
+                repeat = transverse(node,i,visited,path)
+            else:
+                repeat=True            
+            
+            #path.extend(visited)
+            #visited=[]
+            """
+            if index in visited:
+                visited.remove(index) #remove index from visited list
+                path.append(index) #add index to path
+            """
+    
+    return repeat
+
+def transverse(node,index,visited=[], path=[]):
+    sink=False
+    repeat=False
+
+    currentnode=node[index]
+
+    if index not in visited:
+            visited.append(index)           
+    
+    if currentnode.adj==[]:
+        sink=True
+        path.append(index)
+        if index in visited:
+                visited.remove(index)
+    else:        
+        for i in currentnode.adj:            
+            if repeat:
+                break
+            if i not in visited and repeat==False and i != index: #looping back to itself is cycle
+                if index not in visited:
+                    visited.append(index)
+                repeat = transverse(node,i,visited,path)
+            else:
+                repeat=True            
+            
+            #path.extend(visited)
+            #visited=[]
+            path.append(index)
+            if index in visited:
+                visited.remove(index)
+            """
+            if index in visited:
+                visited.remove(index) #remove index from visited list
+                path.append(index) #add index to path
+            """
+    
+    return repeat
+
+def acyclic(adj):   #return 1 if cycle found, 0 if no cycle found
+
+    num_vertices,num_edges=number_of_components(adj)
+    
+
+
+    node=[]
+    for i in range(num_vertices):
+        node.append(graphnodeclass(i))
+    
+    for i in adj:
+        node[i[0]].add_adj(i[1])
+    
+    currentnode=0
+    visited=[]
+
+    repeat=False
+    
+    all_visited=False
+    
+    while repeat==False and all_visited==False:    
+        localvisited=[]    
+        path=[]
+        repeat=transverse(node,currentnode,localvisited,path)
+        visited.extend(path)
+
+        for i in path:
+            #remove nodes visited from the list
+            for j in node:
+                j.remove_adj(i)
+                pass
+        
+        if repeat==False: #only find next node if no cycle found
+            visitied_norepeats=list()
+
+            #only add unique nodes to visitied_norepeats
+            for i in visited:
+                if i not in visitied_norepeats:
+                    visitied_norepeats.append(i)
+
+            if len(visitied_norepeats)==num_vertices:
+                all_visited=True
+            else:
+                #find next node to transverse
+                for i in range(num_vertices):
+                    if i not in visitied_norepeats:
+                        currentnode=i
+                        break
+
+    #print (visited)
+    #print (repeat)
+        
+    return 1 if repeat==True else 0
+
+
+def move_vertex(vertex, src, dst):
+    src.remove(vertex)
+    dst.append(vertex)
+
+
+def acyclic_reference(adj):
+    finish = []
+    current = []
+    set = [x for x in range(len(adj))]
+
+    def explore(v):
+        move_vertex(v, set, current)
+        for u in adj[v]:
+            if u in finish:
+                continue
+            if u in current:
+                return True
+            if explore(u):
+                return True
+        move_vertex(v, current, finish)
+        return False
+
+    while len(set) > 0:
+        if explore(set[0]):
+            return 1
     return 0
 
+
 if __name__ == '__main__':
+
+
+    num_vertices, num_edges = (int(i) for i in input().split())
+    vertices_split = [(input().split()) for _ in range(num_edges)]
+    edges = [[int(i[0]) - 1, int(i[1]) - 1] for i in vertices_split]
+    print(acyclic(edges))
+    """
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
@@ -16,3 +196,4 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
     print(acyclic(adj))
+    """
--- graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py
+++ graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py
@@ -7,13 +7,266 @@ def dfs(adj, used, order, x):
     pass
 
 
-def toposort(adj):
+class graphnodeclass:
+    def __init__(self,n):
+        self.adj=[]     
+        self.reverse_adj=[]   
+        self.removed=False  #flags to indicate if node has been removed
+        self.index=n    #index of node in the list
+        self.exists=False
+    def add_adj(self,adj):
+        if adj not in self.adj: #do not do duplicate adj
+            self.adj.append(adj)
+    def add_reverse_adj(self,adj):
+        if adj not in self.reverse_adj:
+            self.reverse_adj.append(adj)
+
+    def is_sink(self):
+        sink=False
+        if len(self.adj)<=0:
+            sink=True        
+        return sink
+    
+    def is_source(self):
+        source=False
+        if len(self.reverse_adj)<=0:
+            source=True
+        return source
+
+    def remove_adj(self,n):
+        if n in self.adj:
+            self.adj.remove(n)
+        if n in self.reverse_adj:
+            self.reverse_adj.remove(n)
+
+
+def number_of_components(adj):
+    num_edges=len(adj)
+    num_vertices=-1
+    for i in adj:
+        for j in i:
+            if j>num_vertices:
+                num_vertices=j
+    num_vertices+=1
+
+    return num_vertices,num_edges
+
+def transverse(node,index,visited=[], path=[]):
+    sink=False
+    repeat=False
+
+    currentnode=node[index]
+
+    if index not in visited:
+            visited.append(index)           
+    
+    if currentnode.adj==[]:
+        sink=True
+        path.append(index)
+        if index in visited:
+                visited.remove(index)
+    else:        
+        for i in currentnode.adj:            
+            if repeat:
+                break
+            if i not in visited and repeat==False and i != index: #looping back to itself is cycle
+                if index not in visited:
+                    visited.append(index)
+                repeat = transverse(node,i,visited,path)
+            else:
+                repeat=True            
+            
+            #path.extend(visited)
+            #visited=[]
+            path.append(index)
+            if index in visited:
+                visited.remove(index)
+    
+    return repeat
+
+def transverse_dfs(node,index,path=[]):  #assume graph is DAG/acyclic
+    sink=False
+    
+    while sink==False:
+        currentnode=node[index]
+        
+        if len (currentnode.adj)<=0:
+            sink=True
+            path.append(index)                    
+            for i in range(len(node)):      #need to optimize this loop
+                node[i].remove_adj(index) #remove index from all nodes                
+        else:        
+            nextnode=currentnode.adj[0]
+            transverse_dfs(node,nextnode,path) #if not DAG will cycle forever!    
+    return sink
+
+
+def get_edges(nodes):
+    edges=[]
+    for index in range(len(nodes)):
+        i=nodes[index]
+        for j in i:
+            edges.append([index,j])
+    return edges
+
+def toposort_bak1(adj): #adj is nodes
     used = [0] * len(adj)
     order = []
     #write your code here
-    return order
+    nodes=list(adj)
+
+    num_vertices=len(adj)
+
+
+    edges=get_edges(nodes)
+
+    #num_vertices,num_edges=number_of_components(adj)   
+
+    node=[]
+    for i in range(num_vertices):
+        node.append(graphnodeclass(i))
+        #if i in vertices:
+        #    node[i].exists=True
+        if i>=0:
+            node[i].exists=True
+    
+    for i in edges:
+        node[i[0]].add_adj(i[1])
+    
+    currentnode=0
+    visited=[]
+
+    repeat=False
+    
+    all_visited=False
+    
+    while repeat==False and all_visited==False:    
+        localvisited=[]    
+        path=[]
+        repeat=transverse(node,currentnode,localvisited,path)
+        
+        pathreverse=path[::-1]
+        visited=pathreverse+visited
+
+        for i in path:
+            #remove nodes visited from the list
+            for j in node:
+                j.remove_adj(i)
+                pass
+        
+        if repeat==False: #only find next node if no cycle found
+            visitied_norepeats=list()
+
+            #only add unique nodes to visitied_norepeats
+            for i in visited:
+                if i not in visitied_norepeats:
+                    visitied_norepeats.append(i)
+
+            if len(visitied_norepeats)==num_vertices:
+                all_visited=True
+            else:
+                #find next node to transverse
+                for i in range(num_vertices):
+                    if i not in visitied_norepeats:
+                        currentnode=i
+                        break
+                   
+    path=[]
+
+    for i in visited:
+        if i not in path:
+            path.append(i)
+
+    #for i in range(0,len(path)):
+    #    path[i]+=1
+    return path
+
+def toposort(adj): #adj is nodes
+    used = [0] * len(adj)
+    order = []
+    #write your code here
+    nodes=list(adj)
+
+    num_vertices=len(adj)
+
+
+    edges=get_edges(nodes)
+
+    reversed_edges=[]
+    for i in edges:
+        reversed_edges.append([i[1],i[0]])
+
+    
+
+    node=[]
+    for i in range(num_vertices):
+        node.append(graphnodeclass(i))
+        #if i in vertices:
+        #    node[i].exists=True
+        if i>=0:
+            node[i].exists=True
+    
+    for i in edges:
+        node[i[0]].add_adj(i[1])
+    
+    for i in reversed_edges:
+        node[i[0]].add_reverse_adj(i[1])
+    
+    currentnode=0
+
+    all_visited=False
+
+    path=[]
+    currentnode=0
+
+    while node[currentnode].is_source()==False and currentnode<num_vertices-1:  #start with source
+        currentnode+=1
+
+    #find node that
+    while all_visited==False:
+        visited=[]
+        unvisited=[]
+        
+        sink=transverse_dfs(node,currentnode,visited)
+
+        path=visited[::-1]+path
+    
+        #check if all nodes visited
+        for i in range(0,num_vertices):     #need to optimize this loop
+            if i not in path:
+                unvisited.append(i)
+        
+        if len(unvisited)==0:
+            all_visited=True
+        else:                     
+            for i in range(0,len(unvisited)):
+                currentnode=unvisited[i]
+                if node[unvisited[i]].is_source()==True:                    
+                    break
+            assert node[currentnode].is_source()==True, 'currentnode is not source'
+    #path_reverse=path[::-1]
+
+    return path
+    
+    
+    
+    
+
+
 
 if __name__ == '__main__':
+    
+    num_vertices, num_edges = (int(i) for i in input().split())
+    vertices_split = [(input().split()) for _ in range(num_edges)]
+    edges = [[int(i[0]) - 1, int(i[1]) - 1] for i in vertices_split]
+    adj = [[] for _ in range(num_vertices)]
+    for (a, b) in edges:
+        adj[a].append(b)
+    order = toposort(adj)
+    for x in order:
+        print(x + 1, end=' ')
+    
+    """
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
@@ -25,4 +278,4 @@ if __name__ == '__main__':
     order = toposort(adj)
     for x in order:
         print(x + 1, end=' ')
-
+    """
